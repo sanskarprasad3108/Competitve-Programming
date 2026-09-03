80c77a4c3c73d144bef31cd0edb0df499349592d
sanskar prasad <anshujsr1999@gmail.com>
2026-09-03 19:34:31 +0530

    Time: 2 ms (100%), Space: 121.8 MB (44.2%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,50 +1,9 @@
-import java.util.*;
-
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int n = nums1.length;
-
-        Integer smallestEven = null;
-        Integer smallestOdd = null;
-
-        for (int num : nums1) {
-            if (num % 2 == 0) {
-                if (smallestEven == null || num < smallestEven)
-                    smallestEven = num;
-            } else {
-                if (smallestOdd == null || num < smallestOdd)
-                    smallestOdd = num;
-            }
-        }
-
-        // Try making all EVEN
-        boolean canEven = true;
-        if (smallestEven == null) canEven = false;
-        else {
-            for (int num : nums1) {
-                if (num % 2 != 0) { // odd → need odd smaller
-                    if (smallestOdd == null || smallestOdd >= num) {
-                        canEven = false;
-                        break;
-                    }
-                }
-            }
-        }
-
-        // Try making all ODD
-        boolean canOdd = true;
-        if (smallestOdd == null) canOdd = false;
-        else {
-            for (int num : nums1) {
-                if (num % 2 == 0) { // even → need smaller odd
-                    if (smallestOdd >= num) {
-                        canOdd = false;
-                        break;
-                    }
-                }
-            }
-        }
-
-        return canEven || canOdd;
+        int min=Integer.MAX_VALUE;
+        for(int i:nums1)min=Math.min(min,i);
+        if(min%2!=0)return true;
+        for(int i:nums1)if(i%2!=0)return false;
+        return true;
     }
 }
